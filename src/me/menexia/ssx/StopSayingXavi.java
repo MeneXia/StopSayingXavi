9f4e3e8182e289f15c51548cc3b69d698191f8ce
Xavi Ablaza <xlablaza@gmail.com>
2011-12-02 20:16:21 +0800

    need to test

---
+++ src/me/menexia/ssx/StopSayingXavi.java
@@ -0,0 +1,40 @@
+package me.menexia.ssx;
+import java.util.logging.Logger;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerListener;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+public class StopSayingXavi extends JavaPlugin {
+	public static StopSayingXavi plugin;
+	public final Logger logger = Logger.getLogger("Minecraft");
+	@Override
+	public void onDisable() {
+		this.logger.info("StopSayingXavi disabled!");
+	}
+	@Override
+	public void onEnable() {
+		PluginManager pm = this.getServer().getPluginManager();
+		pm.registerEvent(Event.Type.PLAYER_CHAT, new playerListener(), Event.Priority.Monitor, this);
+		PluginDescriptionFile pdf = this.getDescription();
+		this.logger.info( pdf.getName() + " version" + pdf.getVersion() + " by MeneXia is enabled!" );
+	}
+	class playerListener extends PlayerListener {
+		@Override
+		public void onPlayerChat(PlayerChatEvent love) {
+			if (love.isCancelled()) return;
+		String message = love.getMessage();
+		Player onlyyoumica = love.getPlayer();
+			if (message.contains("xavi")) {
+			love.setCancelled(true);
+			onlyyoumica.getLocation().getWorld().strikeLightningEffect(null); // might have to do a onlyyoumica.getLocation() instead of null
+			onlyyoumica.damage(19);
+			onlyyoumica.sendMessage(ChatColor.RED + "Message blocked by MeneXia.");
+			onlyyoumica.sendMessage(ChatColor.RED + "Don't make me send down lightning again!");
+			}
+		}
+	}
+}
